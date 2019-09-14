8071dd4e3e487cc5a1a7fbfd02376e82f3656abc
Yuriy Syrovetskiy <cblp@cblp.su>
2019-09-14 13:53:34 +0300

    Add Position, Velocity

--- Main.hs
+++ Main.hs
@@ -1,4 +1,20 @@
--- import Apecs
+{-# LANGUAGE TypeFamilies #-}
+
+import Apecs
+-- import Apecs.Gloss
+-- import Control.Monad
+-- import Graphics.Gloss.Data.Bitmap
+import Linear
+-- import System.Exit
+-- import System.Random
+
+newtype Position = Position (V2 Float) deriving (Show)
+
+instance Component Position where type Storage Position = Map Position
+
+newtype Velocity = Velocity (V2 Float) deriving (Show)
+
+instance Component Velocity where type Storage Velocity = Map Velocity
 
 main :: IO ()
 main = pure ()
