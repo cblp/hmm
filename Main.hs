2cdb98fe7074338eb83a463c8db6ef4e2882bf43
Yuriy Syrovetskiy <cblp@cblp.su>
2019-09-14 14:02:13 +0300

    Add some stuff

--- Main.hs
+++ Main.hs
@@ -1,12 +1,17 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
 
-import Apecs
--- import Apecs.Gloss
--- import Control.Monad
--- import Graphics.Gloss.Data.Bitmap
-import Linear
 -- import System.Exit
 -- import System.Random
+-- import Control.Monad
+-- import Graphics.Gloss.Data.Bitmap
+import Apecs
+import Apecs.Gloss
+import Linear
 
 newtype Position = Position (V2 Float) deriving (Show)
 
@@ -16,5 +21,74 @@ newtype Velocity = Velocity (V2 Float) deriving (Show)
 
 instance Component Velocity where type Storage Velocity = Map Velocity
 
+newtype Time = Time Float deriving (Show, Num)
+
+instance Semigroup Time where (<>) = (+)
+
+instance Monoid Time where mempty = 0
+
+instance Component Time where type Storage Time = Global Time
+
+data Player = Player deriving (Show)
+
+instance Component Player where type Storage Player = Unique Player
+
+makeWorld
+  "World"
+  [ ''Position,
+    ''Velocity,
+    ''Time,
+    ''Player,
+    ''Camera
+    ]
+
+type System' a = System World a
+
+-- type Kinetic = (Position, Velocity)
+
 main :: IO ()
-main = pure ()
+main = do
+  -- assets <- loadAssets
+  w <- initWorld
+  runWith w $ do
+    initialize
+    play
+      (InWindow "Shmup" (worldWidth, worldHeight) (10, 10))
+      black
+      60
+      draw {- assets -}
+      handleEvent
+      step
+
+initialize :: System' ()
+initialize = do
+  -- _player <- newEntity (Player, Position playerStartPos, Velocity 0)
+  pure ()
+
+worldWidth, worldHeight :: Int
+worldWidth = 600
+worldHeight = 800
+
+draw :: System' Picture
+draw = pure mempty
+
+handleEvent :: Event -> System' ()
+handleEvent = \case
+  -- EventKey (SpecialKey KeyLeft) Down _ _ ->
+  --   cmap $ \(Player, Velocity (V2 x _)) -> Velocity (V2 (x - playerSpeed) 0)
+  -- EventKey (SpecialKey KeyLeft) Up _ _ ->
+  --   cmap $ \(Player, Velocity (V2 x _)) -> Velocity (V2 (x + playerSpeed) 0)
+  -- EventKey (SpecialKey KeyRight) Down _ _ ->
+  --   cmap $ \(Player, Velocity (V2 x _)) -> Velocity (V2 (x + playerSpeed) 0)
+  -- EventKey (SpecialKey KeyRight) Up _ _ ->
+  --   cmap $ \(Player, Velocity (V2 x _)) -> Velocity (V2 (x - playerSpeed) 0)
+  -- EventKey (SpecialKey KeySpace) Down _ _ ->
+  --   cmapM_ $ \(Player, pos :: Position) -> do
+  --     _bullet <- newEntity (Bullet, pos, Velocity (V2 0 bulletSpeed))
+  --     pure ()
+  -- EventKey (SpecialKey KeyEsc) Down _ _ -> liftIO exitSuccess
+  _ -> pure ()
+
+step :: Float -> System' ()
+step _dT = do
+  pure ()
